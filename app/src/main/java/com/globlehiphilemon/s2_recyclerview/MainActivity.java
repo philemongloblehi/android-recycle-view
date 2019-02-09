dfc317dc890d822bc04d57b923721b0a215863a8
GLOBLEHI PHILEMON <philemon.globlehi@akiltechnologies.com>
2019-02-09 10:00:08 +0000

    Initial commit

---
+++ app/src/main/java/com/globlehiphilemon/s2_recyclerview/MainActivity.java
@@ -0,0 +1,66 @@
+package com.globlehiphilemon.s2_recyclerview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.globlehiphilemon.s2_recyclerview.model.Personne;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtNom;
+    EditText txtPrenoms;
+    EditText txtCommune;
+    Button btnSave;
+    Button btnCancel;
+    Toolbar toolbar;
+
+    Personne personne;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.toolbar = findViewById(R.id.toolbar);
+        this.toolbar.setTitle(R.string.title);
+        setSupportActionBar(this.toolbar);
+
+        this.txtNom = findViewById(R.id.txt_nom);
+        this.txtPrenoms = findViewById(R.id.txt_prenoms);
+        this.txtCommune = findViewById(R.id.txt_commune);
+        this.btnSave = findViewById(R.id.btn_save);
+        this.btnCancel = findViewById(R.id.btn_cancel);
+
+        this.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                personne = new Personne(
+                        "0001",
+                        txtNom.getText().toString(),
+                        txtPrenoms.getText().toString(),
+                        txtCommune.getText().toString()
+                );
+
+                Toast.makeText(MainActivity.this, personne.toString(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        this.btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtNom.setText("");
+                txtPrenoms.setText("");
+                txtCommune.setText("");
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/globlehiphilemon/s2_recyclerview/model/Personne.java
@@ -0,0 +1,58 @@
+package com.globlehiphilemon.s2_recyclerview.model;
+
+public class Personne {
+
+    private String code;
+    private String nom;
+    private String prenoms;
+    private String commune;
+
+    public Personne(String code, String nom, String prenoms, String commune) {
+        this.code = code;
+        this.nom = nom;
+        this.prenoms = prenoms;
+        this.commune = commune;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenoms() {
+        return prenoms;
+    }
+
+    public void setPrenoms(String prenoms) {
+        this.prenoms = prenoms;
+    }
+
+    public String getCommune() {
+        return commune;
+    }
+
+    public void setCommune(String commune) {
+        this.commune = commune;
+    }
+
+    @Override
+    public String toString() {
+        return "Personne{" +
+                "code='" + code + '\'' +
+                ", nom='" + nom + '\'' +
+                ", prenoms='" + prenoms + '\'' +
+                ", commune='" + commune + '\'' +
+                '}';
+    }
+}
