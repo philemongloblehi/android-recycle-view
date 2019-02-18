10ddd46843bb74078db590ef1ce8bc2d71438b78
GLOBLEHI PHILEMON <philemon.globlehi@akiltechnologies.com>
2019-02-18 20:36:34 +0000

    Affichage des details d'un item

--- app/src/main/java/com/globlehiphilemon/s2_recyclerview/adapter/MyAdapter.java
+++ app/src/main/java/com/globlehiphilemon/s2_recyclerview/adapter/MyAdapter.java
@@ -1,7 +1,9 @@
 package com.globlehiphilemon.s2_recyclerview.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +18,11 @@ import java.util.ArrayList;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     private Context context;
-    ArrayList<Personne> listPersonne;
+    private final ArrayList<Personne> listPersonne;
+
+    Dialog dialog;
+    TextView txtNomPrenomDetail;
+    TextView txtCommuneDetail;
 
     public MyAdapter(Context context, ArrayList<Personne> listPersonne) {
         this.context = context;
@@ -26,7 +32,37 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return new MyViewHolder(LayoutInflater.from(this.context).inflate(R.layout.item_personne, viewGroup, false));
+        View view;
+        view = LayoutInflater.from(this.context).inflate(R.layout.item_personne, viewGroup, false);
+        final MyViewHolder myViewHolder = new MyViewHolder(view);
+
+        //dialog
+        this.dialog = new Dialog(context);
+        this.dialog.setContentView(R.layout.detail_personne);
+
+
+        myViewHolder.itemCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Toast.makeText(context, "PERSONNE N 00" + String.valueOf(myViewHolder.getAdapterPosition()), Toast.LENGTH_LONG).show();
+
+                txtNomPrenomDetail = dialog.findViewById(R.id.txt_nom_prenoms_detail);
+                txtCommuneDetail = dialog.findViewById(R.id.txt_commune_detail);
+
+                int pos = myViewHolder.getAdapterPosition();
+
+                Personne personne = listPersonne.get(pos);
+
+                txtNomPrenomDetail.setText(personne.getNom() + personne.getPrenoms());
+                txtCommuneDetail.setText(personne.getCommune());
+
+                dialog.show();
+
+            }
+        });
+
+        return myViewHolder;
     }
 
     @Override
@@ -45,13 +81,19 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView txtNomPrenomItem, txtCommuneItem, txtCodeItem;
+        TextView txtNomPrenomItem;
+        TextView txtCommuneItem;
+        TextView txtCodeItem;
+
+        CardView itemCardView;
 
         public MyViewHolder(View view) {
             super(view);
             this.txtCodeItem = view.findViewById(R.id.txt_code_item);
             this.txtCommuneItem = view.findViewById(R.id.txt_commune_item);
             this.txtNomPrenomItem = view.findViewById(R.id.txt_nom_prenom_item);
+
+            this.itemCardView = view.findViewById(R.id.item_cardView);
         }
     }
 
