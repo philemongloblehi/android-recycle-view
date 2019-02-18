2da976f226a9ab8e5bd92c4443ba17ad8a92464d
GLOBLEHI PHILEMON <philemon.globlehi@akiltechnologies.com>
2019-02-18 23:50:53 +0000

    Implementation affichage password

--- app/src/main/java/com/globlehiphilemon/s2_recyclerview/PreferenceActivity.java
+++ app/src/main/java/com/globlehiphilemon/s2_recyclerview/PreferenceActivity.java
@@ -2,6 +2,10 @@ package com.globlehiphilemon.s2_recyclerview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 
@@ -10,6 +14,7 @@ public class PreferenceActivity extends AppCompatActivity {
     private EditText txtNomWifi;
     private EditText txtPassword;
     private Switch switchEtat;
+    private CheckBox showPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,20 @@ public class PreferenceActivity extends AppCompatActivity {
         this.txtNomWifi = findViewById(R.id.txt_nom_wifi);
         this.txtPassword = findViewById(R.id.txt_password);
         this.switchEtat = findViewById(R.id.switch_etat);
+        this.showPassword = findViewById(R.id.show_password);
+
+        showPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (!isChecked) {
+                    // show password
+                    txtPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                } else {
+                    // hide password
+                    txtPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+            }
+        });
     }
 }
